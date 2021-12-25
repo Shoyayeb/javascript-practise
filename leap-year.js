64137d59ea398125735029626fd58ae7ab22a4dc
Shoyayeb <sa.fine@outlook.com>
2021-12-25 11:49:49 +0600

    leap year done

--- leap-year.js
+++ leap-year.js
@@ -1,9 +1,16 @@
 const inputYear = prompt("Enter Year")
 function isLeap(inputYear) {
-    if (inputYear) {
-        if (inputYear % 4 === 0 || inputYear % 400 === 0 || inputYear % 100 !== 0) {
-            alert("leap year");
-        }
-    }
+    // if (inputYear) {
+    //     if (inputYear % 4 === 0 || inputYear % 400 === 0 || inputYear % 100 !== 0) {
+    //         alert("leap year");
+    //     }
+    // }
+    if (inputYear % 4 === 0) {
+        if (inputYear % 100 === 0) {
+            if (inputYear % 400 === 0) {
+                alert("Leap Year");
+            } else { alert("Normal Year"); }
+        } else { alert("Leap Year"); }
+    } else { alert("Normal Year"); }
 }
 isLeap(inputYear);
