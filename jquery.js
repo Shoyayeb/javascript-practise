acfadb2e05a74f9e9f0990be794fbae021f24d5c
Shoyayeb <sa.fine@outlook.com>
2021-12-26 08:31:57 +0600

    jquery

--- jquery.js
+++ jquery.js
@@ -1,2 +1,17 @@
-$("h1").css("color", "greenyellow");
-$("h1").addClass("cssClass");
+// 180 no module
+
+
+// $("h1").css("color", "greenyellow");
+console.log($("h1").hasClass("primary-color"));
+console.log($("h1").css("margin"));
+
+
+// click
+$("button").click(() => {
+    $("h1").text("this is text from jquery");
+    $("h1").addClass("primary-color");
+})
+console.log(document.querySelectorAll("button"));
+document.querySelectorAll("button").onClick = () => {
+
+}
